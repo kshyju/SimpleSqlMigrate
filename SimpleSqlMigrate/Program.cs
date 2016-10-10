bcf2d74c6452a1539698e85da5bc6c99bc5d09e2
Shyju <connectshyju@gmail.com>
2016-10-09 20:18:11 -0400

    Fixing a stupid "transaction nolonger in use" error!

--- SimpleSqlMigrate/Program.cs
+++ SimpleSqlMigrate/Program.cs
@@ -189,8 +189,18 @@ end";
             }
             catch (Exception ex)
             {
-                if (trans != null)
+                try
+                {
                     trans.Rollback();
+                }
+                catch (Exception ex2)
+                {
+                    // This catch block will handle any errors that may have occurred
+                    // on the server that would cause the rollback to fail, such as
+                    // a closed connection.
+                    Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                    Console.WriteLine("  Message: {0}", ex2.Message);
+                }
                 Trace.TraceError(ex.Message);
                 throw;
             }
